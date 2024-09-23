d5492a33e7fc7d0d1d152d0c68544c3d04009656
Pedro Amoedo <pedroantoniomouraamoedo@gmail.com>
2024-09-23 16:22:34 -0300

    Add files via upload

---
+++ static/JavaScript/perguntas.js
@@ -0,0 +1,25 @@
+const palavras = [
+    "Hazel",
+    "Augustus",
+    "Câncer",
+    "Amsterdam",
+    "Granada",
+    "Amor",
+    "Van Houten",
+    "Imperial",
+    "Isaac",
+    "Estrelas",
+];
+
+const dicas = [
+    "Protagonista feminina do livro",
+    "Personagem masculino e interesse amoroso de Hazel",
+    "Doença enfrentada por Hazel e outros personagens",
+    "Cidade onde Hazel e Augustus fazem uma viagem importante",
+    "Augustus usa esta palavra para descrever a si mesmo",
+    "Sentimento central na relação entre Hazel e Augustus",
+    "Autor fictício de 'Uma Aflição Imperial'",
+    "Parte do título do livro favorito de Hazel",
+    "Melhor amigo de Augustus",
+    "Referência ao título do livro",
+];
